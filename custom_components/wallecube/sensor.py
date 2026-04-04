15f8255b9ad17cfa65101c66c90e068421b1ed63
SegidaO <149874717+SegidaO@users.noreply.github.com>
2026-04-04 16:44:55 +0300

    Update sensor.py

--- custom_components/wallecube/sensor.py
+++ custom_components/wallecube/sensor.py
@@ -1,30 +1,41 @@
+import logging
+
 from homeassistant.components.sensor import (
     SensorEntity,
     SensorDeviceClass,
     SensorStateClass,
 )
+from homeassistant.core import callback
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
+
 from .const import DOMAIN, DEVICE_MODEL, MANUFACTURER, SENSOR_TYPES
 
-import logging
 _LOGGER = logging.getLogger(__name__)
 
+
 async def async_setup_entry(hass, entry, async_add_entities):
-    sensors = []
-    for sensor_type in SENSOR_TYPES:
-        sensors.append(WalleCubeSensor(entry.data["device_id"], sensor_type, SENSOR_TYPES[sensor_type]))
+    """Set up WalleCube sensors from a config entry."""
+    device_id = entry.data["device_id"]
+    sensors: list[WalleCubeSensor] = []
+
+    for sensor_type, cfg in SENSOR_TYPES.items():
+        sensors.append(WalleCubeSensor(device_id, sensor_type, cfg))
+
     async_add_entities(sensors)
 
+
 class WalleCubeSensor(SensorEntity):
-    def __init__(self, device_id, sensor_id, config):
+    """Representation of a WalleCube sensor."""
+
+    def __init__(self, device_id, sensor_id, config) -> None:
         self._device_id = device_id
         self._sensor_id = sensor_id
         self._config = config
         self._state = None
-        self._attr_name = self._config['name']
+        self._attr_name = self._config["name"]
 
     @property
-    def unique_id(self):
+    def unique_id(self) -> str:
         return f"{DOMAIN}_{self._device_id}_{self._sensor_id}"
 
     @property
@@ -34,56 +45,57 @@ class WalleCubeSensor(SensorEntity):
             "identifiers": {(DOMAIN, self._device_id)},
             "name": DEVICE_MODEL,
             "manufacturer": MANUFACTURER,
-            "sw_version": '0.0.1'
+            "sw_version": "0.0.1",
         }
 
     @property
     def has_entity_name(self) -> bool:
         """Indicate that entity has name defined."""
-
         return True
 
     @property
     def state_class(self) -> SensorStateClass:
         """Return the type of state class."""
-
-        return self._config['state_class']
+        return self._config["state_class"]
 
     @property
     def device_class(self) -> SensorDeviceClass:
         """Return entity device class."""
-
-        return self._config['device_class']
+        return self._config["device_class"]
 
     @property
     def native_unit_of_measurement(self) -> str:
         """Return the native unit."""
-
-        return self._config['unit']
+        return self._config["unit"]
 
     @property
     def icon(self) -> str:
-        """Set icon."""
-        if self._sensor_id == 'batteryCapacity':
-            if self._state == None:
+        """Return icon."""
+        if self._sensor_id == "batteryCapacity":
+            if self._state is None:
                 return f"{self._config['icon']}-unknown"
             battery_life = max(10, min(int(round(self._state / 10) * 10), 100))
             if battery_life >= 100:
-                return self._config['icon']
+                return self._config["icon"]
             return f"{self._config['icon']}-{battery_life}"
-        return self._config['icon']
+        return self._config["icon"]
 
     @property
-    def state(self):
+    def native_value(self):
         return self._state
 
-    async def async_added_to_hass(self):
+    async def async_added_to_hass(self) -> None:
+        """Register callbacks."""
         self.async_on_remove(
             async_dispatcher_connect(
-                self.hass, self.unique_id, self.update_state
+                self.hass,
+                self.unique_id,
+                self._async_update_state,
             )
         )
 
-    def update_state(self, new_state):
+    @callback
+    def _async_update_state(self, new_state) -> None:
+        """Handle state update from dispatcher."""
         self._state = new_state
-        self.hass.loop.call_soon_threadsafe(self.async_write_ha_state)
+        self.async_write_ha_state()
